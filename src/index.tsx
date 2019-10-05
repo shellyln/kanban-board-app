3ef49e9aa7bb4c7cbc7c7f2cc221cbd0cfe39d76
shellyln <shellyln@users.noreply.github.com>
2019-10-05 19:43:04 +0900

    [FIX] service worker is not registered due to load event handler registration timing

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,7 @@ import getAppStore,
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: https://bit.ly/CRA-PWA
 
-    serviceWorker.unregister();
-    // serviceWorker.register();
 })();
+
+// serviceWorker.unregister();
+serviceWorker.register();
