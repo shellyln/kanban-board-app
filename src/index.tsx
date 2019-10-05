9f16b8c9519b471956f957aa1cc4f51ff01daae7
shellyln <shellyln@users.noreply.github.com>
2019-10-05 19:45:05 +0900

    Fix comment

--- src/index.tsx
+++ src/index.tsx
@@ -26,11 +26,11 @@ import getAppStore,
         </Provider>,
         document.getElementById('root'));
 
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: https://bit.ly/CRA-PWA
-
 })();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 
 // serviceWorker.unregister();
 serviceWorker.register();
